33bd4bda0af97d201894cfbbea9a813605d96509
praveensqln <71185528+praveensqln@users.noreply.github.com>
2022-09-29 15:23:12 -0400

    Update streamlit_app.py
    Added another title

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text( ':Oatmeal: Omega 3 and Blueberry Oatmeal')
 streamlit.text( ':Spinach: Kale, Spinach and Rocket Smoothie')
 streamlit.text( ':Egg: Hard-Boiled Free-Range Egg')
+streamlit.test(' Avacado tost')
+
+streamlit.header(' Build your own Fruit Smoothie ')
