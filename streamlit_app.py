a1e00fcb442f3630b4542db3015ba114c7b13cdf
praveensqln <71185528+praveensqln@users.noreply.github.com>
2022-09-29 15:27:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title('My Parents New Health Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text( 'Omega 3 and Blueberry Oatmeal')
+streamlit.text( '	🌸:  Omega 3 and Blueberry Oatmeal')
 streamlit.text( 'Kale, Spinach and Rocket Smoothie')
 streamlit.text( 'Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado tost')
