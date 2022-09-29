c39d6aa1c9925d18fa07dc206fae3445f2848271
praveensqln <71185528+praveensqln@users.noreply.github.com>
2022-09-29 15:26:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text( 'Omega 3 and Blueberry Oatmeal')
 streamlit.text( 'Kale, Spinach and Rocket Smoothie')
 streamlit.text( 'Hard-Boiled Free-Range Egg')
-streamlit.teXt('Avacado tost')
+streamlit.text('Avacado tost')
 
