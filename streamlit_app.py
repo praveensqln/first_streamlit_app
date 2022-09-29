7dcaed091c3c0ec2d7179571564c5c7977751866
praveensqln <71185528+praveensqln@users.noreply.github.com>
2022-09-29 15:55:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ streamlit.text( ' 🥚: Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑: Avacado tost')
 
 streamlit.header(' 🍌  Build your own Fruit Smoothie 🍓 ')
+
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
