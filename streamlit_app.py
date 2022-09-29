e90fb6cb758ec493b19bb7f7579c1f60f36ecb0f
praveensqln <71185528+praveensqln@users.noreply.github.com>
2022-09-29 15:10:28 -0400

    Update streamlit_app.py
    Emijo code syntax error redeem.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My Parents New Health Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text( :Oatmeal: 'Omega 3 and Blueberry Oatmeal')
-streamlit.text(:Spinach: 'Kale, Spinach and Rocket Smoothie')
-streamlit.text(:Egg: 'Hard-Boiled Free-Range Egg')
+streamlit.text( ':Oatmeal: Omega 3 and Blueberry Oatmeal')
+streamlit.text( ':Spinach: Kale, Spinach and Rocket Smoothie')
+streamlit.text( ':Egg: Hard-Boiled Free-Range Egg')
