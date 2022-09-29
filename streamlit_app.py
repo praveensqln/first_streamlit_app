6036b4c49d9c3d03f6ef577b6accfd490a007103
praveensqln <71185528+praveensqln@users.noreply.github.com>
2022-09-29 15:24:46 -0400

    Update streamlit_app.py
    solve Errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,8 @@ import streamlit
 streamlit.title('My Parents New Health Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text( ':Oatmeal: Omega 3 and Blueberry Oatmeal')
-streamlit.text( ':Spinach: Kale, Spinach and Rocket Smoothie')
-streamlit.text( ':Egg: Hard-Boiled Free-Range Egg')
-streamlit.test(' Avacado tost')
-
-streamlit.header(' Build your own Fruit Smoothie ')
+streamlit.text( 'Omega 3 and Blueberry Oatmeal')
+streamlit.text( 'Kale, Spinach and Rocket Smoothie')
+streamlit.text( 'Hard-Boiled Free-Range Egg')
+streamlit.test('Avacado tost')
+streamlit.header('Build your own Fruit Smoothie')
